5b972c2dbc1e1568dbf146fa32d86e456f9fae49
Vuek0 <vueko.eruko@gmail.com>
2024-07-04 14:06:00 +0500

    https

--- src/App.vue
+++ src/App.vue
@@ -77,7 +77,7 @@ async function registrationHandler(e) {
     };
     try {
       const req = await axios.post(
-        `http://to-do-list-server-lake.vercel.app/api/users?key=${API_KEY}`,
+        `https://to-do-list-server-lake.vercel.app/api/users?key=${API_KEY}`,
         data
       );
       const res = await req;
